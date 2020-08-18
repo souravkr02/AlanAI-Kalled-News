0ac03abbe6acf143ebab7d3021ba1a96b5597281
Sourav Kumar <sourav.kumar@outlook.com>
2020-08-18 22:34:27 +0530

    fixed logo and modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography, Divider, Chip, TextField, Button } from '@material-ui/core';
 import SimpleModal from '@material-ui/core/Modal';
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
 
 import useStyles from './styles';
 
@@ -12,26 +12,26 @@ const Modal = ({ isOpen, setIsOpen, showFeedback }) => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'alan_ai', e.target, 'user_dhVImkgxaL27bxQ8pLPQ5')
-      .then((result) => { console.log(result.text); }, (error) => { console.log(error.text); });
+    // emailjs.sendForm('gmail', 'alan_ai', e.target, 'user_dhVImkgxaL27bxQ8pLPQ5')
+    //   .then((result) => { console.log(result.text); }, (error) => { console.log(error.text); });
   };
 
   if (isOpen && showFeedback) {
-    body = (
-      <div className={classes.paper}>
-        <Typography variant="h3">Something went wrong? Send us some feedback</Typography>
-        <br />
-        <form className={classes.form} onSubmit={sendEmail}>
-          <TextField name="name" label="Your name" variant="outlined" />
-          <br />
-          <TextField name="email" type="email" label="Your email" variant="outlined" />
-          <br />
-          <TextField name="feedback" multiline rows={5} helperText="Describe the problems that you've encontered." label="Feedback" variant="outlined" />
-          <br />
-          <Button type="submit" variant="outlined" color="primary">Send</Button>
-        </form>
-      </div>
-    );
+    // body = (
+    //    <div className={classes.paper}>
+    //      <Typography variant="h3">Something went wrong? Send us some feedback</Typography>
+    //      <br />
+    //      <form className={classes.form} onSubmit={sendEmail}>
+    //        <TextField name="name" label="Your name" variant="outlined" />
+    //        <br />
+    //        <TextField name="email" type="email" label="Your email" variant="outlined" />
+    //        <br />
+    //        <TextField name="feedback" multiline rows={5} helperText="Describe the problems that you've encontered." label="Feedback" variant="outlined" />
+    //        <br />
+    //        <Button type="submit" variant="outlined" color="primary">Send</Button>
+    //      </form>
+    //    </div>
+    // );
   } else {
     body = (
       <div className={classes.paper}>
