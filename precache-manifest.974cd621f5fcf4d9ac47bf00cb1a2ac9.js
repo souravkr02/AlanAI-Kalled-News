e26302de9ff63cbf67e2c823a46b9b5737b80160
Sourav Kumar <sourav.kumar@outlook.com>
2020-08-31 16:40:57 +0530

    Updates

---
+++ precache-manifest.974cd621f5fcf4d9ac47bf00cb1a2ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d7dfc7319818845343973fb0c6e605",
+    "url": "/AlanAI-Kalled-News/index.html"
+  },
+  {
+    "revision": "275d15defca4f1ff42da",
+    "url": "/AlanAI-Kalled-News/static/css/main.3595e86f.chunk.css"
+  },
+  {
+    "revision": "75ed80008ec25721cafb",
+    "url": "/AlanAI-Kalled-News/static/js/2.9b5e81d5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AlanAI-Kalled-News/static/js/2.9b5e81d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275d15defca4f1ff42da",
+    "url": "/AlanAI-Kalled-News/static/js/main.527a7bdf.chunk.js"
+  },
+  {
+    "revision": "3d93ff1491d5a13ddd23",
+    "url": "/AlanAI-Kalled-News/static/js/runtime-main.1d9ab7a6.js"
+  },
+  {
+    "revision": "389c652f1f8c3cf52ad9ae029f941750",
+    "url": "/AlanAI-Kalled-News/static/media/logo.389c652f.PNG"
+  }
+]);
